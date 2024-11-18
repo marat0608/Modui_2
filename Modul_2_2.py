53dcb176ae6a6b812948c7cfcfd3400ccd573c38
marat0608 <marat0608@gmail.com>
2024-11-18 23:22:54 +0300

    Add files via upload

---
+++ Modul_2_2.py
@@ -0,0 +1,10 @@
+first = input("Введите число first: ")
+second = input("введите число second: ")
+third = input("введите число third: ")
+print(first, second, third)
+if first == second == third:
+    print('Все равны')
+elif first != second != third:
+    print('Равных нет')
+else:
+    print('Равны только 2 числа')
